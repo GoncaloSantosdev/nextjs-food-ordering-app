018aeb9fb4028afd91a1e22cb326e6300b9c7f22
GoncaloSantosdev <goncalosantosprofissional@gmail.com>
2023-08-28 09:24:34 +0100

    adding user to the db

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,12 +1,21 @@
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions, getServerSession } from "next-auth";
+// Prisma
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "./connect";
 // Providers
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
+};
+
+// Allows getting user and status on server components and API routes
+export const getAuthSession = () => {
+  getServerSession(authOptions);
 };
